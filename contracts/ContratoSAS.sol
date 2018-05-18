14a88580196f543d89b33e25f51894d452ac72f8
Bruno Calmels <bruno.calmels@macherit.com>
2018-05-18 17:16:40 -0300

    feat: El script sripts/contracts.sh permite crear, compilar y migrar un contrato nuevo pasándole un ID

--- contracts/ContratoSAS.sol
+++ contracts/ContratoSAS.sol
@@ -6,6 +6,11 @@ contract ContratoSAS is mortal {
     /* Events */
     event contributionFiled(address indexed from, uint256 indexed uid, uint256 amount);
     event receivedFunds(address _from, uint256 _amount);
+    event nombreSet(string nombre);
+    event montoSet(uint256 monto);
+    event fechaSet(string fecha);
+    event descripcionSet(string descripcion);
+    event cuitSet(uint cuit);
 
     struct Contribution {
         address _from;
@@ -14,6 +19,11 @@ contract ContratoSAS is mortal {
     }
 
     uint contribution_counter;
+    string  m_nombre;
+    uint256 m_monto;
+    string  m_fecha;
+    string  m_descripcion;
+    uint    m_cuit;
 
     mapping(uint => Contribution) m_contributions;
 
@@ -28,6 +38,36 @@ contract ContratoSAS is mortal {
         contribution_counter++;
         m_contributions[contribution_counter] = Contribution(msg.sender, uid, msg.value);
         emit contributionFiled(msg.sender, uid, msg.value);
+    }
+
+    /* Setea el nombre de la SAS/proyecto */
+    function setNombre(string nombre) onlyowner public {
+        m_nombre = nombre;
+        emit nombreSet(m_nombre);
+    }
+
+    /* Setea el monto de la SAS/proyecto */
+    function setMonto(uint256 monto) onlyowner public {
+        m_monto = monto;
+        emit montoSet(m_monto);
+    }
+
+    /* Setea la fecha de la SAS/proyecto */
+    function setFecha(string fecha) onlyowner public {
+        m_fecha = fecha;
+        emit fechaSet(m_fecha);
+    }
+
+    /* Setea la descripcion de la SAS/proyecto */
+    function setDescripcion(string descripcion) onlyowner public {
+        m_descripcion = descripcion;
+        emit descripcionSet(m_descripcion);
+    }
+
+    /* Setea el CUIT de la SAS/proyecto */
+    function setCuit(uint cuit) onlyowner public {
+        m_cuit = cuit;
+        emit cuitSet(m_cuit);
     }
 
     /* TODO: Fill in function or get from frontend */
